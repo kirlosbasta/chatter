283f9683966c4116f9b361e59b33f84e3d5d8931
kirlos basta <kirlosbasta@gmail.com>
2024-09-16 22:17:06 +0300

    Add routes to create and delete one to one chat and groups

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,7 +1,75 @@
-import asyncHandler from 'express-async-handler';
 import validator from 'validator';
+import asyncHandler from 'express-async-handler';
+import mongoose from 'mongoose';
 import ChatModel from '../models/chat.model.js';
+import UserModel from '../models/user.model.js';
+import ChatMessageModel from '../models/message.model.js';
+import ChatterError from '../utils/ChatterError.js';
 
+/**
+ * @description -  populate users and lastMessage fileds in chatModel
+ */
+const populateChat = [
+  {
+    $lookup: {
+      from: 'users',
+      localField: 'users',
+      foreignField: '_id',
+      as: 'users',
+      pipeline: [
+        {
+          $project: {
+            password: 0,
+            resetPasswordToken: 0,
+            __v: 0,
+          },
+        },
+      ],
+    },
+  },
+  {
+    $lookup: {
+      from: 'chatmessages',
+      localField: 'lastMessage',
+      foreignField: '_id',
+      as: 'lastMessage',
+      pipeline: [
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'sender',
+            foreignField: '_id',
+            as: 'sender',
+            pipeline: [
+              {
+                $project: {
+                  username: 1,
+                  email: 1,
+                },
+              },
+            ],
+          },
+        },
+        {
+          $addFields: { sender: { $first: '$sender' } },
+        },
+        {
+          $project: {
+            __v: 0,
+          },
+        },
+      ],
+    },
+  },
+  {
+    $addFields: { lastMessage: { $first: '$lastMessage' } },
+  },
+  {
+    $project: {
+      __v: 0,
+    },
+  },
+];
 /**
  * @param {import('express').Request} req - request object
  * @param {import('express').Response} res - response object
@@ -9,7 +77,6 @@ import ChatModel from '../models/chat.model.js';
  * @description - get all user previos chats group or one to one
  */
 const getAllChatsController = asyncHandler(async (req, res) => {
-  console.log('inside get All Controller');
   const allChats = await ChatModel.aggregate([
     {
       $match: {
@@ -21,64 +88,194 @@ const getAllChatsController = asyncHandler(async (req, res) => {
         updatedAt: -1,
       },
     },
-    {
-      $lookup: {
-        from: 'users',
-        localField: 'users',
-        foreignField: '_id',
-        as: 'users',
-        pipeline: [
-          {
-            $project: {
-              password: 0,
-              resetPasswordToken: 0,
-              __v: 0,
-            },
-          },
-        ],
-      },
-    },
-    {
-      $lookup: {
-        from: 'chatmessages',
-        localField: 'lastMessage',
-        foreignField: '_id',
-        as: 'lastMessage',
-        pipeline: [
-          {
-            $lookup: {
-              from: 'users',
-              localField: 'sender',
-              foreignField: '_id',
-              as: 'sender',
-              pipeline: [
-                {
-                  $project: {
-                    username: 1,
-                    email: 1,
-                  },
-                },
-              ],
-            },
-          },
-          {
-            $addFields: { sender: { $first: '$sender' } },
-          },
-        ],
-      },
-    },
-    {
-      $addFields: { lastMessage: { $first: '$lastMessage' } },
-    },
+    ...populateChat,
   ]);
-  console.log(allChats);
   res.status(200);
   return res.json(allChats);
 });
-// retrive a chat with a user if exist and create if not
-// delete chat with all it's messages
+
+/**
+ * @param {import('express').Request} req - request object
+ * @param {import('express').Response} res - response object
+ *
+ * @description - get a chat between a user and a another user
+ * by userId of the receiver and if no chat exist create new one
+ */
+const getOrCreateChatController = asyncHandler(async (req, res) => {
+  const { receiverId } = req.params;
+  // check if the id is valid mongo id if not throw not found error
+  if (!validator.isMongoId(receiverId)) {
+    throw new ChatterError(404, 'User is Not Found');
+  }
+  const receiver = await UserModel.findById(receiverId);
+  if (!receiver) {
+    throw new ChatterError(404, 'User is Not Found');
+  }
+  if (receiver._id.toString() === req.user._id.toString()) {
+    throw new ChatterError(400, "You can't chat with yourself");
+  }
+  // if vaild id look for an existing one to one chat with user and receiver
+  const existingChat = await ChatModel.aggregate([
+    {
+      $match: {
+        isGroupChat: false,
+        $and: [
+          {
+            users: {
+              $elemMatch: { $eq: new mongoose.Types.ObjectId(receiverId) },
+            },
+          },
+          {
+            users: {
+              $elemMatch: { $eq: req.user._id },
+            },
+          },
+        ],
+      },
+    },
+    ...populateChat,
+  ]);
+  // if exists send the chat with 200 status
+  if (existingChat.length) {
+    return res.status(200).json(existingChat[0]);
+  }
+  // if doesn't exit create a chat and send it back with 201 status
+  const newChat = await ChatModel.create({
+    name: req.user.username,
+    isGroupChat: false,
+    users: [req.user._id, receiver._id],
+    admin: req.user._id,
+  });
+
+  // populate users fields
+  const populatedNewChat = await ChatModel.aggregate([
+    {
+      $match: {
+        _id: newChat._id,
+      },
+    },
+    ...populateChat,
+  ]);
+  return res.status(201).json(populatedNewChat[0]);
+});
+
+/**
+ * @param {import('express').Request} req - request object
+ * @param {import('express').Response} res - response object
+ *
+ * @description - delete chat with all it's messages
+ */
+const deleteChatController = asyncHandler(async (req, res) => {
+  // check if chat id is valid
+  const { chatId } = req.params;
+  if (!validator.isMongoId(chatId)) {
+    throw new ChatterError(404, 'Chat is Not Found');
+  }
+
+  // check if the chat id exist
+  const chat = await ChatModel.find({
+    _id: new mongoose.Types.ObjectId(chatId),
+    isGroupChat: false,
+  });
+  if (chat.length === 0) {
+    throw new ChatterError(404, 'Chat is Not Found');
+  }
+
+  // check if the user is one of the users
+  if (!chat[0].users.includes(req.user._id)) {
+    throw new ChatterError(403, 'You are not part of the chat');
+  }
+  // delete all the messages connected to the chat id
+  await ChatMessageModel.deleteMany({
+    chat: chat[0]._id,
+  });
+  // delete the chat and send 200 status code
+  await ChatModel.deleteOne({ _id: chat[0]._id });
+  return res
+    .status(200)
+    .json({ success: true, message: 'Chat deleted successfuly' });
+});
 // create a group chat
+/**
+ * @param {import('express').Request} req - request object
+ * @param {import('express').Response} res - response object
+ *
+ * @description - create a group chat
+ */
+const createGroupController = asyncHandler(async (req, res) => {
+  // get the name from body
+  const { name } = req.body;
+
+  // if not exist throw bad requset
+  if (!name) {
+    throw new ChatterError(400, 'Missing Group name');
+  }
+
+  // check if the name already exist with user as admin
+  const existingGroup = await ChatModel.findOne({
+    name,
+    isGroupChat: true,
+    admin: req.user._id,
+  });
+  // if exist return 400 bad request
+  if (existingGroup) {
+    throw new ChatterError(400, 'You already has a group with this name');
+  }
+  // create the chat and add the user as admin and one of the users
+  const newGroup = await ChatModel.create({
+    name,
+    isGroupChat: true,
+    admin: req.user._id,
+    users: [req.user._id],
+  });
+  const payload = await ChatModel.aggregate([
+    {
+      $match: {
+        _id: newGroup._id,
+      },
+    },
+    ...populateChat,
+  ]);
+  return res.status(201).json(payload[0]);
+});
 // add myself to group chat
+/**
+ * @param {import('express').Request} req - request object
+ * @param {import('express').Response} res - response object
+ *
+ * @description - add the user to a group chat
+ */
+const addUserToGroupController = asyncHandler(async (req, res) => {
+  // check if group id is valid mongo id
+  const { groupId } = req.params;
+  if (!validator.isMongoId(groupId)) {
+    throw new ChatterError(404, 'User Not Found');
+  }
+  // check if group exists
+  const groupChat = await ChatModel.findById(groupId);
+  if (!groupChat) throw new ChatterError(404, 'User Not Found');
+  // check if the user already in the group
+  if (groupChat.users.includes(req.user._id)) {
+    throw new ChatterError(400, 'You are already in the group');
+  }
+  // add the user to the group and return the populated group
+  await ChatModel.findByIdAndUpdate(groupChat._id, {
+    $push: { users: req.user._id },
+  });
+  const [payload] = await ChatModel.aggregate([
+    {
+      $match: { _id: groupChat._id },
+    },
+    ...populateChat,
+  ]);
+  return res.status(200).json(payload);
+});
 // leave group chat
 // delete a group chat if i'm the admin with all the messages
-export { getAllChatsController };
+export {
+  getAllChatsController,
+  getOrCreateChatController,
+  deleteChatController,
+  createGroupController,
+  addUserToGroupController,
+};
--- src/routes/chats.routes.js
+++ src/routes/chats.routes.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import passport from 'passport';
-import { getAllChatsController } from '../controllers/chat.controller.js';
+import {
+  getAllChatsController,
+  getOrCreateChatController,
+  deleteChatController,
+  createGroupController,
+  addUserToGroupController,
+} from '../controllers/chat.controller.js';
 
 const router = Router();
 
 router.use(passport.authenticate('jwt', { session: false }));
 router.get('/chats', getAllChatsController);
+router.get('/chats/:receiverId', getOrCreateChatController);
+router.delete('/chats/:chatId', deleteChatController);
+router.post('/groups', createGroupController);
+router.post('/groups/:groupId', addUserToGroupController);
 
 export default router;
